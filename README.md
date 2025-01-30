3df66e44a48556c3582d619eda59f679f19968e1
Josue Martinez <jmartinezolivares1@gmail.com>
2025-01-29 18:16:32 -0600

    Setup sample

---
+++ README.md
@@ -0,0 +1,5 @@
+# Sample Page
+---
+This fake website is designed to deploy and show how the a sample of any website would look like
+
+> The price must start in USD 100 per website
---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+
+
+export const App = () => {
+  return (
+    <>
+      <h1>Welcome to the Sample page</h1>
+      <hr />
+      <p>Occaecat sint ad qui irure ad eu esse velit sunt ea eiusmod laboris. Reprehenderit ad minim ex eiusmod labore anim sunt amet consequat aliqua. Ad laborum do mollit et quis qui. Irure Lorem laborum incididunt ad ullamco nisi nisi ut quis labore nulla incididunt.</p>
+    </>
+  )
+}
